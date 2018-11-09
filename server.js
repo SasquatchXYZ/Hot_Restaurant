bbdc598e1dc707205e86e13cd09bf0828e1384d7
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-11-09 01:14:05 -0500

    updated a few things

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const path = require('path');
 
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+// Data ================================================================================================================
+
 const tables = [];
 
 const waiting = [];
-
-
-
-
 
 // Routes ===============================================================================
 app.get('/', function(req, res) {
